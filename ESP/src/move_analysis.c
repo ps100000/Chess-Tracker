86c67273047cec8966f07bf0b95d5641af8d5674
Fuchsdaemon <67006570+Fuchsdaemon@users.noreply.github.com>
2020-06-17 10:52:59 +0200

    Update move_analysis.c

--- ESP/src/move_analysis.c
+++ ESP/src/move_analysis.c
@@ -4,6 +4,5 @@
 void analyse_move(field_state_change_t state[8][8], char* logline_buf){
     memset(logline_buf, '\0', LOGLINE_BUF_SIZE);
     //static field_state_t last_state[8][8];
-    // hsallo
 
 };
