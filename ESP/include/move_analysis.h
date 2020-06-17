dbeae91cc91630c236b20f4b559c46df09f853c6
Fuchsdaemon <67006570+Fuchsdaemon@users.noreply.github.com>
2020-06-17 18:33:46 +0200

    Add files via upload

--- ESP/include/move_analysis.h
+++ ESP/include/move_analysis.h
@@ -1,17 +1,22 @@
 #ifndef _MOVE_ANALYSIS_H_
 #define _MOVE_ANALYSIS_H_
-
-#define LOGLINE_BUF_SIZE 10
-
-enum field_state_change{
+/*----------includes----------*/
+/*----------------------------*/
+/*----------macros----------*/
+#define LOGLINE_BUF_SIZE    10
+/*--------------------------*/
+/*----------globals----------*/
+typedef enum field_state_change
+{
     FIELD_NO_CHANGE,
     FIELD_EMPTY,
     FIELD_WHITE_PIECE,
     FIELD_BLACK_PIECE
-};
-
-typedef enum field_state_change field_state_change_t;
-
-void analyse_move(field_state_change_t state[8][8], char* logline_buf);
-
+} field_state_change_t;
+/*---------------------------*/
+/*----------functions----------*/
+char isCastling(field_state_change_t state[8][8], char notationString[7]);
+void analyse_move(field_state_change_t stateChange[8][8], char* logline_buf);
+void isCheck(field_state_change_t state[8][8]);
+/*-----------------------------*/
 #endif
