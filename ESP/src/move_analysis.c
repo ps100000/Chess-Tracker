2036a033397fe513f4a6b885cd505034ffcc481c
Fuchsdaemon <67006570+Fuchsdaemon@users.noreply.github.com>
2020-06-18 12:07:44 +0200

    Add files via upload

--- ESP/src/move_analysis.c
+++ ESP/src/move_analysis.c
@@ -1,149 +1,90 @@
 /*----------includes----------*/
 #include <memory.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include "move_analysis.h"
+#include "..\include\move_analysis.h"
+#include "..\include\storage.h"
 /*----------------------------*/
-/*----------globals----------*/
-static char completeField[8][8] =
-{
-	'T','S','L','D','K','L','S','T',
-	'B','B','B','B','B','B','B','B',
-	'0','0','0','0','0','0','0','0',
-	'0','0','0','0','0','0','0','0',
-	'0','0','0','0','0','0','0','0',
-	'0','0','0','0','0','0','0','0',
-	'B','B','B','B','B','B','B','B',
-	'T','S','L','D','K','L','S','T'
-};
-/*---------------------------*/
-void analyse_move(field_state_change_t stateChange[8][8], char* logline_buf)
+void analyse_move(field_state_change_t state[8][8], char* logline_buf)
 {
     /*----------locals----------*/
-    bool isFigureBeforeEmpty = false;
-    char piece = ' ', fieldLetter, notationString[] = "", temp[] = "";
+    bool isPieceBeforeEmpty = false;
+    char piece = ' ', fieldLetter, temp[LOGLINE_BUF_SIZE] = "";
     int pieceField[2] = { 0, 0 }, changes = 0;
     static field_state_change_t last_stateChange[8][8];
+	static char completeField[8][8] =
+	{
+		'T','S','L','D','K','L','S','T',
+		'B','B','B','B','B','B','B','B',
+		'0','0','0','0','0','0','0','0',
+		'0','0','0','0','0','0','0','0',
+		'0','0','0','0','0','0','0','0',
+		'0','0','0','0','0','0','0','0',
+		'B','B','B','B','B','B','B','B',
+		'T','S','L','D','K','L','S','T'
+	};
     /*--------------------------*/
     /*----------clear buffer----------*/
     memset(logline_buf, '\0', LOGLINE_BUF_SIZE);
     /*--------------------------------*/
-    for (int i = 0, j = 8; i < 8; i++, j--)
+    for (int y = 0, j = 8; y < 8; y++, j--)
 	{
-		for (int k = 0; k < 8; k++)
+		for (int x = 0; x < 8; x++)
 		{
 			/*----------change number into a letter----------*/
-			switch (k)
-			{
-				case 0:
-				{
-					fieldLetter = 'A';
-					break;
-				}
-				case 1:
-				{
-					fieldLetter = 'B';
-					break;
-				}
-				case 2:
-				{
-					fieldLetter = 'C';
-					break;
-				}
-				case 3:
-				{
-					fieldLetter = 'D';
-					break;
-				}
-				case 4:
-				{
-					fieldLetter = 'E';
-					break;
-				}
-				case 5:
-				{
-					fieldLetter = 'F';
-					break;
-				}
-				case 6:
-				{
-					fieldLetter = 'G';
-					break;
-				}
-				case 7:
-				{
-					fieldLetter = 'H';
-					break;
-				}
-				default:
-				{
-					break;
-				}
-			}
+			fieldLetter = 'A' + x;
 			/*-----------------------------------------------*/
-			if (FIELD_NO_CHANGE == state[i][k])
+			if (FIELD_NO_CHANGE == state[y][x])
 				continue;
-			else if (FIELD_EMPTY == state[i][k])
+			else if (FIELD_EMPTY == state[y][x])
 			{
 				changes++;
-				piece = completeField[i][k];
+				piece = completeField[y][x];
 
-				if (true == isFigureBeforeEmpty)
+				if (isPieceBeforeEmpty)
 				{
 					completeField[pieceField[0]][pieceField[1]] = piece;
-					isFigureBeforeEmpty = false;
+					isPieceBeforeEmpty = false;
 				}
 
 				if ('B' == piece)
-                {
-					notationString[] = ' ';
-                }
+					memset(logline_buf, '\0', LOGLINE_BUF_SIZE);
 				else
-					notationString[] = piece;
+					logline_buf[0] = piece;
 
-				char buffer[2];
-				_itoa_s(j, buffer, 2, 10);
+				logline_buf[strlen(logline_buf)] = fieldLetter;
+				logline_buf[strlen(logline_buf)] = j + '0';
+				memcpy(logline_buf + strlen(logline_buf), temp, strlen(temp));
 
-				notationString[7] = notationString + fieldLetter + buffer[2] + temp[7];
-
-				completeField[i][k] = '0';
+				completeField[y][x] = '0';
 			}
-			else if ((FIELD_WHITE_PIECE == state[i][k]) || (FIELD_BLACK_PIECE == state[i][k]))
+			else if ((FIELD_WHITE_PIECE == state[y][x]) || (FIELD_BLACK_PIECE == state[y][x]))
 			{
 				changes++;
-				if ("" == notationString)
+				if (!strlen(logline_buf))
 				{
-					if ('0' == completeField[i][k])
-						temp[7] = '-';
+					if ('0' == completeField[y][x])
+						temp[0] = '-';
 					else
-						temp[7] = 'X';
+						temp[0] = 'X';
 
-					char buffer[2];
-					_itoa_s(j, buffer, 2, 10);
-
-					temp[7] = temp + fieldLetter + buffer;
-
-					pieceField[0] = i;
-					pieceField[1] = k;
-
-					isFigureBeforeEmpty = true;
+					temp[strlen(temp)] = fieldLetter;
+					temp[strlen(temp)] = j + '0';
+					pieceField[0] = y;
+					pieceField[1] = x;
+					isPieceBeforeEmpty = true;
 				}
 				else
 				{
-					if ('0' == completeField[i][k])
-						notationString[7] += '-';
+					if ('0' == completeField[y][x])
+						logline_buf[strlen(logline_buf)] = '-';
 					else
-						notationString[7] += 'X';
+						logline_buf[strlen(logline_buf)] += 'X';
 
-					char buffer[2];
-					_itoa_s(j, buffer, 2, 10);
-
-					notationString[7] = notationString + fieldLetter + buffer;
-
-					completeField[i][k] = piece;
-
-					isFigureBeforeEmpty = false;
+					logline_buf[strlen(logline_buf)] = fieldLetter;
+					logline_buf[strlen(logline_buf)] = j + '0';
+					completeField[y][x] = piece;
+					isPieceBeforeEmpty = false;
 				}
 			}
 		}
@@ -151,31 +92,24 @@ void analyse_move(field_state_change_t stateChange[8][8], char* logline_buf)
 
     if (changes >= 3)
 	{
-		notationString[7] = "";
+		memset(logline_buf, '\0', LOGLINE_BUF_SIZE);
 		changes = 0;
-		notationString[7] = isCastling(state, notationString);
+		isCastling(state, logline_buf, completeField);
 	}
-	/*else
-		isCheck(state);*/
     
-    // write file
-	/*fstream f;
-	f.open("chess_notation.txt", ios::app);
-	f << notationString << "\n";
-	f.close();*/
+    /*----------write file----------*/
+	FILE *fp;
+	fp = fopen("/sdcard/chess_notation.txt", "a");
 
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            printf("%c", completeField[i][j]);
-        }
-
-        printf("\n");
-    }
+	if (NULL == fp)
+		printf("file couldn't be opened");
+	else
+		fprintf(fp, "%s\n", logline_buf);
+	fclose(fp);
+	/*------------------------------*/
 }
 
-char isCastling(field_state_change_t stateChange[8][8], char notationString[7])
+void isCastling(field_state_change_t state[8][8], char* const logline_buf, char completeField[8][8])
 {
 	if ((FIELD_EMPTY == state[0][0]) && (FIELD_BLACK_PIECE == state[0][2]) &&
 		(FIELD_BLACK_PIECE == state[0][3]) && (FIELD_EMPTY == state[0][4]))
@@ -184,7 +118,7 @@ char isCastling(field_state_change_t stateChange[8][8], char notationString[7])
 		completeField[0][2] = 'K';
 		completeField[0][3] = 'T';
 		completeField[0][4] = '0';
-		notationString = "0-0-0";
+		memcpy(logline_buf, "0-0-0", 5);
 	}
 	else if ((FIELD_EMPTY == state[7][0]) && (FIELD_WHITE_PIECE == state[7][2]) &&
 		(FIELD_WHITE_PIECE == state[7][3]) && (FIELD_WHITE_PIECE == state[7][4]))
@@ -193,7 +127,7 @@ char isCastling(field_state_change_t stateChange[8][8], char notationString[7])
 		completeField[7][2] = 'K';
 		completeField[7][3] = 'T';
 		completeField[7][4] = '0';
-		notationString = "0-0-0";
+		memcpy(logline_buf, "0-0-0", 5);
 	}
 	else if ((FIELD_EMPTY == state[0][4]) && (FIELD_BLACK_PIECE == state[0][5]) &&
 		(FIELD_BLACK_PIECE == state[0][6]) && (FIELD_EMPTY == state[0][7]))
@@ -202,7 +136,7 @@ char isCastling(field_state_change_t stateChange[8][8], char notationString[7])
 		completeField[0][5] = 'T';
 		completeField[0][6] = 'K';
 		completeField[0][7] = '0';
-		notationString = "0-0";
+		memcpy(logline_buf, "0-0", 3);
 	}
 	else if ((FIELD_EMPTY == state[7][4]) && (FIELD_WHITE_PIECE == state[7][5]) &&
 		(FIELD_WHITE_PIECE == state[7][6]) && (FIELD_EMPTY == state[7][7]))
@@ -211,64 +145,6 @@ char isCastling(field_state_change_t stateChange[8][8], char notationString[7])
 		completeField[7][5] = 'T';
 		completeField[7][6] = 'K';
 		completeField[7][7] = '0';
-		notationString = "0-0";
-	}
-
-	return notationString;
-}
-
-void isCheck(field_state_change_t stateChange[8][8])
-{
-	/*----------locals----------*/
-	int chord[2] = { 0, 0 };
-	/*--------------------------*/
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			if ((FIELD_WHITE_PIECE == state[i][j]) || (FIELD_BLACK_PIECE == state[i][j]))
-			{
-				chord[0] = i;
-				chord[1] = j;
-			}
-		}
-	}
-
-	switch (completeField[chord[0]][chord[1]])
-	{
-		case 'B':
-		{
-			break;
-		}
-
-		case 'T':
-		{
-			break;
-		}
-
-		case 'L':
-		{
-			break;
-		}
-
-		case 'S':
-		{
-			break;
-		}
-
-		case 'D':
-		{
-			break;
-		}
-
-		case 'K':
-		{
-			break;
-		}
-
-		default:
-		{
-			break;
-		}
+		memcpy(logline_buf, "0-0", 3);
 	}
 }
---
+++ ESP/src/storage.c
@@ -0,0 +1,20 @@
+#include "..\include\storage.h"
+
+void storage_init()
+{
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+    slot_config.gpio_miso = 2;
+    slot_config.gpio_mosi = 15;
+    slot_config.gpio_sck  = 14;
+    slot_config.gpio_cs   = 13;
+
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 4,
+        .allocation_unit_size = 0
+    };
+
+    sdmmc_card_t* card;
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+}
