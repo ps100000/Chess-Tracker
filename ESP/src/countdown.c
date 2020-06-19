eeaff6d6e8e521652f16bac26f0bae7829a9f2ef
Fuchsdaemon <67006570+Fuchsdaemon@users.noreply.github.com>
2020-06-19 12:10:03 +0200

    Add files via upload

--- ESP/src/countdown.c
+++ ESP/src/countdown.c
@@ -4,11 +4,14 @@
 #include "..\include\countdown.h"
 /*----------------------------*/
 /*----------macros----------*/
-#define STACK_SIZE      512
-#define TIME_PER_PLAYER 1800000
-#define PUSH_PIN        16
-#define HIGH            1
-#define LOW             0
+#define STACK_SIZE              512
+#define TIME_PER_PLAYER_IN_MS   1800000
+#define STOPTIME_IN_S           3
+#define STOPTIME_IN_MS          STOPTIME_IN_S * 1000
+#define CCT_TICKS_PER_US 	    CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ
+#define PUSH_PIN                16
+#define HIGH                    1
+#define LOW                     0
 /*--------------------------*/
 /*----------globals----------*/
 TaskHandle_t xHandle = NULL;
@@ -20,47 +23,79 @@ BaseType_t xReturned = xCreateTask(
     tskIDLE_PRIORITY,
     &xHandle
 );
+bool isGameStarted = true;
 /*---------------------------*/
 /*----------init----------*/
-gpio_set_direction(PUSH_PIN,GPIO_MODE_INPUT);
+gpio_set_direction(PUSH_PIN, GPIO_MODE_INPUT);
 attachInterrupt(PUSH_PIN, changePlayer, HIGH);
 /*------------------------*/
 void timer()
 {
     /*----------locals----------*/
-    const TickType_t xDelay = 10 / portTICK_PERIOD_MS;
-    int time_player1 = TIME_PER_PLAYER, time_player2 = TIME_PER_PLAYER;
     bool itsPlayerOnesTurn = true;
+    int time_player1 = TIME_PER_PLAYER_IN_MS, time_player2 = TIME_PER_PLAYER_IN_MS;
+    uint32_t cctMarker = 0, now = 0, exactlyTimeInMS = 0;
+    const TickType_t xDelay = 1 / portTICK_PERIOD_MS;
     /*--------------------------*/
     while (1)
     {
         if (itsPlayerOnesTurn)
         {
-            time_player1 -= xDelay;
-            printf("%i", time_player1);
+            time_player1 -= exactlyTimeInMS;
+            printf("%i\n", time_player1);
         }
         else
         {
-            time_player2 -= xDelay;
-            printf("%i", time_player2);
+            time_player2 -= exactlyTimeInMS;
+            printf("%i\n", time_player2);
         }
 
+        if (time_player1 <= 0)
+        {
+            printf("Player 1 lost");
+            isGameStarted = false;
+        }
+        else if (time_player2 <= 0)
+        {
+            printf("Player 2 lost");
+            isGameStarted = false;
+        }
+
+        cctMarker = XTHAL_GET_CCOUNT();
+
         vTaskDelay(xDelay);
+
+        now = XTHAL_GET_CCOUNT();
+
+        if (cctMarker <= now)
+            exactlyTimeInMS = (((now - cctMarker)+CCT_TICKS_PER_US/2)/CCT_TICKS_PER_US)/1000;
+        else
+            exactlyTimeInMS = ((now + (0xFFFFFFFF - cctMarker) + CCT_TICKS_PER_US/2)/CCT_TICKS_PER_US)/1000;
+        
     }
 }
 
 void changePlayer()
 {
-    /*---???---*/
-        static int PushPinLastState = LOW;
-        int PushPinCurrentState = digitalRead(PUSH_PIN);
+    /*----------locals----------*/
+    static int PushPinLastState = LOW;
+    int PushPinCurrentState = digitalRead(PUSH_PIN), timeToStop = 0;
+    /*--------------------------*/
+    if (PushPinLastState != PushPinCurrentState)
+    {
+        PushPinLastState = PushPinCurrentState;
 
-        if (PushPinLastState != PushPinCurrentState)
+        if (HIGH == digitalRead(PUSH_PIN))
         {
-            PushPinLastState = PushPinCurrentState;
-
-            if (HIGH == digitalRead(PUSH_PIN))
-                itsPlayerOnesTurn = !itsPlayerOnesTurn;
+            while(1)
+            {
+                if (LOW == digitalRead(PUSH_PIN))
+                    itsPlayerOnesTurn = !itsPlayerOnesTurn;
+                timeToStop++;
+                if (STOPTIME_IN_MS == timeToStop)
+                    isGameStarted = false;
+                vTaskDelay(xDelay / 10);
+            }
         }
-        /*---???---*/
+    }
 }
