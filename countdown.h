b01e8e5ef38c97701a6222cd135ade4754fe7a7d
Fuchsdaemon <67006570+Fuchsdaemon@users.noreply.github.com>
2020-06-23 07:10:36 +0200

    Delete countdown.h

--- countdown.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COUNTDOWN_H_
-#define _COUNTDOWN_H_
-/*----------functions----------*/
-void countdown_init();
-void countdown();
-void changePlayer();
-/*-----------------------------*/
-#endif
